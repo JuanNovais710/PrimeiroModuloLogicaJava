2be7e2d8e4f9a005375fa02b38d1a7d49ba85f93
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-04-19 10:58:31 -0300

    Add files via upload

---
+++ src/tudo/ate/o/momento/Exercicio01Metodos.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Exercicio01Metodos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        imprimirTraco();
+        String[] cursos = new String[]{"Java EE", "Spring", "Java OO Avançado"};
+        System.out.println("Escolha dentre os cursos abaixo: ");
+        iterarVetor(cursos);
+
+        System.out.print("O curso que você deseja é o: ");
+        Integer posicaoCursoEscolhido = scanner.nextInt();
+        Boolean posicaoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
+        if (!posicaoValida) {
+            posicaoInvalida();
+        }
+        imprimirTraco();
+        String[] formasPagamento = new String[]{"Cartão", "Boleto"};
+        System.out.println("Escolha dentre as formas de pagamento abaixo: ");
+        iterarVetor(formasPagamento);
+
+        System.out.print("Sua forma de pagamento escolhida é: ");
+        Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
+        posicaoValida = posicaoFormaPagamentoEscolhida >= 0 && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+        if (!posicaoValida) {
+            posicaoInvalida();
+        }
+        String cursoEscolhido = cursos[posicaoCursoEscolhido];
+        String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+        imprimirTraco();
+        System.out.println("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
+        scanner.close();
+    }
+
+    static void imprimirTraco() {
+        String padraoTraco = "------------------------------------------------";
+        System.out.println(padraoTraco);
+    }
+
+    static void posicaoInvalida() {
+        System.err.println("Posição inválida! ");
+        System.exit(1);
+    }
+
+    static void iterarVetor(String[] vetor) {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("[" + i + "] " + vetor[i]);
+        }
+    }
+}
+
