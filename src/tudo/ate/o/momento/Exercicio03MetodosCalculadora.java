3dc32c9ed1637ccedcee74dbf5f7b3907b659628
JuanNovais710 <100390136+JuanNovais710@users.noreply.github.com>
2022-04-22 04:21:22 -0300

    Add files via upload

--- src/tudo/ate/o/momento/Exercicio03MetodosCalculadora.java
+++ src/tudo/ate/o/momento/Exercicio03MetodosCalculadora.java
@@ -1,63 +1,99 @@
-package com.company;
-/*
-Crie uma pequena calculadora de subtração e adição.
-Crie um programa que peça dois números do usuário e também qual a operação ele deseja fazer - adição ou subtração.
-Para cada operação, você deve construir um método que realize a mesma, ou seja, teremos um método para adição,
-que vai receber dois números como parâmetros e retornar o resultado da soma entre eles, e outro método para subtração.
-Fique a vontade para implementar multiplicação e divisão também.
- */
-
 import java.util.Scanner;
 
 public class Exercicio03MetodosCalculadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Integer tipoDeOperacao = receberOperacao("Qual o tipo de operação que deseja realizar?\n(1): Adição\t(2): Subtação", scanner);
-        Boolean operacaoEValida = operacaoValida(tipoDeOperacao);
-        if(operacaoEValida) {
-            if(tipoDeOperacao == 1) {
-                imprimirTexto("Informe o primeiro número que deseja somar: ");
-                Integer primeiroNumero = scanner.nextInt();
-                imprimirTexto("Informe o segundo número que deseja realizar a soma: ");
-                Integer segundoNumero = scanner.nextInt();
-                Integer resultadoDaSoma = adicao(primeiroNumero, segundoNumero);
+        Double primeiroNumeroRecebido = receberNumero(scanner);
+        quebraLinha();
+        Byte operadorEscolhidoPeloUsuario = receberOperacao(scanner);
+        quebraLinha();
+        Double segundoNumeroRecebido = receberNumero(scanner);
+        switch (operadorEscolhidoPeloUsuario) {
+            case 0 -> {
+                char subtrair = '-';
+                Double resultado = realizarCalculo(operadorEscolhidoPeloUsuario, primeiroNumeroRecebido, segundoNumeroRecebido);
+                System.out.print(primeiroNumeroRecebido + " " + subtrair + " " + segundoNumeroRecebido + " = " + resultado);
+            }case 1 -> {
+                char adicionar = '+';
+                Double resultado = realizarCalculo(operadorEscolhidoPeloUsuario, primeiroNumeroRecebido, segundoNumeroRecebido);
+                System.out.print(primeiroNumeroRecebido + " " + adicionar + " " + segundoNumeroRecebido + " = " + resultado);
+            }case 2 -> {
+                char multiplicar = '*';
+                Double resultado = realizarCalculo(operadorEscolhidoPeloUsuario, primeiroNumeroRecebido, segundoNumeroRecebido);
+                System.out.print(primeiroNumeroRecebido + " " + multiplicar + " " + segundoNumeroRecebido + " = " + resultado);
+            }case 3 -> {
+                char dividir = '/';
+                Double resultado = realizarCalculo(operadorEscolhidoPeloUsuario, primeiroNumeroRecebido, segundoNumeroRecebido);
+                System.out.print(primeiroNumeroRecebido + " " + dividir + " " + segundoNumeroRecebido + " = " + resultado);
+            }default -> {
+                posicaoInvalida();
             }
-            else if(tipoDeOperacao == 2) {
-                imprimirTexto("Informe o primeiro número que deseja subtrair: ");
-                Integer primeiroNumero = scanner.nextInt();
-                imprimirTexto("Informe o segundo número que deseja realizar a subtração: ");
-                Integer segundoNumero = scanner.nextInt();
-                Integer resultadoDaSubtracao = subtracao(primeiroNumero, segundoNumero);
-            }
-        }else {
-            posicaoInvalida();
         }
     }
-    static Integer receberOperacao(String texto, Scanner scanner) {
-        imprimirTexto(texto);
-        Integer sinalOperador = scanner.nextInt();
-        return sinalOperador;
-    }
-    static Boolean operacaoValida(Integer numeroDigitado){
-        Boolean validarOperacao = numeroDigitado == 1 || numeroDigitado == 2;
-        return validarOperacao;
-    }
-    static Integer adicao(Integer primeiroNumero, Integer segundoNumero) {
-        int resultadoAdicao = primeiroNumero + segundoNumero;
-        imprimirTexto("O resultado de " + primeiroNumero + " + " + segundoNumero + " é: " + resultadoAdicao);
-        return resultadoAdicao;
-    }
-    static Integer subtracao(Integer primeiroNumero, Integer segundoNumero) {
-        int resultadoSubtracao = primeiroNumero - segundoNumero;
-        imprimirTexto("O resultado de " + primeiroNumero + " - " + segundoNumero + " é: " + resultadoSubtracao);
-        return resultadoSubtracao;
+
+    static Double receberNumero(Scanner scanner) {
+        imprimirTexto();
+        return scanner.nextDouble();
+
     }
 
-    static void imprimirTexto(String texto) {
-        System.out.println(texto);
+    static Byte receberOperacao(Scanner scanner) {
+        imprimeTextoComQuebraDelinha();
+        String[] tiposDeOperacoes = {"Subtração", "adição", "multiplicação", "divisao"};
+        for (int i = 0; i < tiposDeOperacoes.length; i++) {
+            System.out.println("(" + i + ")" + " " + tiposDeOperacoes[i]);
+        }
+        return scanner.nextByte();
+
     }
+
+    static Double realizarCalculo(Byte tipoDeOperacao, Double primeiroNumeroRecebido, Double segundoNumeroRecebido) {
+        Double resultado = 0.0;
+        switch (tipoDeOperacao) {
+            case 0 -> resultado = subtracao(primeiroNumeroRecebido, segundoNumeroRecebido);
+            case 1 -> resultado = adicao(primeiroNumeroRecebido, segundoNumeroRecebido);
+            case 2 -> resultado = multiplicacao(primeiroNumeroRecebido, segundoNumeroRecebido);
+            case 3 -> resultado = divisao(primeiroNumeroRecebido, segundoNumeroRecebido);
+            default -> {
+                posicaoInvalida();
+            }
+        }
+        return resultado;
+    }
+
+    static Double adicao(Double primeiroNumero, Double segundoNumero) {
+        return primeiroNumero + segundoNumero;
+    }
+
+    static Double subtracao(Double primeiroNumero, Double segundoNumero) {
+        return primeiroNumero - segundoNumero;
+    }
+
+    static Double divisao(Double primeiroNumero, Double segundoNumero) {
+        return primeiroNumero / segundoNumero;
+    }
+
+    static Double multiplicacao(Double primeiroNumero, Double segundoNumero) {
+        return primeiroNumero * segundoNumero;
+    }
+
+    static void imprimirTexto() {
+        System.out.print("Informe o número: ");
+    }
+
+    static void imprimeTextoComQuebraDelinha() {
+        System.out.println("Qual o tipo de operação desejada: ");
+    }
+
+    static void quebraLinha() {
+        System.out.println("|--------------------|");
+    }
+
     static void posicaoInvalida() {
         System.err.println("A operação é inválida. ");
         System.exit(1);
     }
 }
+
+
+
